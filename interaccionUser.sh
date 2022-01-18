7cc8c71a1d19ba17195772f79e4e281da456ffab
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-01-18 09:41:17 -0600

    Se agregaron funciones y se hizo ejecutable

--- interaccionUser.sh
+++ interaccionUser.sh
@@ -4,16 +4,30 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-echo "Dime tu nimbre: "     #S]e pregunta por el nombre
-read Nombre                 #De esta forma se guarda en la variable Nombre
+echo "Dime tu nombre: "     #Se pregunta por el nombre
+read -n 12 -d " " Nombre    #De esta forma se guarda en la variable Nombre y se
+                           #restringe una longitud de 12 caracteres con "-n 12" 
+                          #y un delimitador que termine al encontrar un espacio 
+                          #con: -d " "
 echo "Hola ${Nombre}"       #De esta forma se imprime
-read -t 4 -p "Dime tu apellido: " Apellido #Segunda forma en la que se pregunta
-    #y se almacena en una sola linea. Ademas se da un tiempo de 4 segundos 
-    #para responder con "-t 4".
+read -t 6 -p "Dime tu apellido: " Apellido #Segunda forma en la que se pregunta
+    #y se almacena en una sola linea. Ademas se da un tiempo de 6 segundos 
+    #para responder con "-t 6".
+echo                #Este "echo" lo usamos para dejar un salto de linea
 
 if [ -z "${Apellido}" ]; then
     echo "Por que no me quieres decir tu apellido ${Nombre}?"
+    Apellido=${Apellido:=Lopez}
+    echo "Tu apellido ahora es ${Apellido}"
+
 else
     echo "Mucho gusto ${Nombre} ${Apellido}"    
 fi
     
+read -sp "Agrega una contraseña: " Password #Mediante "-sp" el texto introducido 
+echo                                    #al escribir no se despliega
+if [ -z ${Password} ]; then
+    echo "Por que no me quieres decir tu contraseña ${Nombre}?"
+else
+    echo "Contraseña guardada correctamente :)"
+fi
